e06ae675cd3289fed9d88edeb36c88ff0e1c9a10
Cyrille85 <nonvignoncyrille3@gmail.com>
2021-06-28 14:22:15 -0400

    deledec2

--- Sampl.tf
+++ Sampl.tf
@@ -10,19 +10,19 @@ terraform {
   }
 }
 
-provider "aws" {
- profile = "default"
- region  = "us-east-1"
-}
+# provider "aws" {
+#  profile = "default"
+#  region  = "us-east-1"
+# }
 
-resource "aws_instance" "my_app_server" {
- ami           = "ami-0d5eff06f840b45e9"
- instance_type = "t2.micro"
+# resource "aws_instance" "my_app_server" {
+#  ami           = "ami-0d5eff06f840b45e9"
+#  instance_type = "t2.micro"
 
- tags = {
-   Name = "myinstance00"
- }
-}
+#  tags = {
+#    Name = "myinstance00"
+#  }
+# }
 
 # # 1-Create a VPC
 # resource "aws_vpc" "Production" {
