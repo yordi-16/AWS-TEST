51d7c5d88843b8107a4d285f21f7db2a1b3b1f0b
Cyrille85 <nonvignoncyrille3@gmail.com>
2021-07-02 13:20:56 -0400

    miim

--- Sampl.tf
+++ Sampl.tf
@@ -10,17 +10,17 @@ terraform {
   }
 }
 
-# provider "aws" {
-#  profile = "default"
-#  region  = "us-east-1"
-# }
+provider "aws" {
+ profile = "default"
+ region  = "us-east-1"
+}
 
 resource "aws_instance" "my_app_server" {
  ami           = "ami-0d5eff06f840b45e9"
  instance_type = "t2.micro"
 
  tags = {
-   Name = "myinstance25"
+   Name = "myinstance65"
  }
 }
 
